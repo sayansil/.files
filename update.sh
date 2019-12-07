b2dbac5315afb4e632d09cdf31198ac155c6d94a
Sayan <sayan227sil@ymail.com>
2019-12-08 02:26:15 +0530

    Add easy update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+mkdir -p .config/dunst
+cp -ar ~/.config/dunst/dunstrc .config/dunst/
+
+mkdir -p .config/i3
+cp -ar ~/.config/i3/config .config/i3
+
+mkdir -p .config/i3blocks
+cp -ar ~/.config/i3blocks/i3blocks.conf .config/i3blocks
+
+mkdir -p .config/polybar
+cp -ar ~/.config/polybar/config .config/polybar
+
+mkdir -p .config/powerline
+cp -ar ~/.config/powerline/* .config/powerline
+
+mkdir -p .config/rofi
+cp -ar ~/.config/rofi/* .config/rofi
+
+mkdir -p .config/termite
+cp -ar ~/.config/termite/config .config/termite
